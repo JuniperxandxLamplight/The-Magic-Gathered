f58880a2295da6e2d741e77f75e017dbfa394ccf
Student <example@example.com>
2019-04-09 11:55:51 -0700

    build button styling for card filter menu

--- src/app/search-data/search-data.component.ts
+++ src/app/search-data/search-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FilterButton } from '../models/search-data-buttons.model'
 
 @Component({
   selector: 'app-search-data',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-data.component.scss']
 })
 export class SearchDataComponent implements OnInit {
-  searchTerms: any[];
+  searchTerms: any[] = [];
 
   constructor() { }
 
@@ -15,6 +16,25 @@ export class SearchDataComponent implements OnInit {
 
   addSearchFilter(item){
     this.searchTerms.push(item);
+    console.log(this.searchTerms)
   }
+
+  colorBtns = [
+    new FilterButton("white", "../../assets/images/white_symbol.svg"),
+    new FilterButton("green", "../../assets/images/green_symbol.svg"),
+    new FilterButton("red", "../../assets/images/red_symbol.svg"),
+    new FilterButton("blue", "../../assets/images/blue_symbol.svg"),
+    new FilterButton("black", "../../assets/images/black_symbol.svg"),
+  ]
+
+  typeBtns = [
+    new FilterButton("artifact", "../../assets/images/artifact_symbol.svg"),
+    new FilterButton("creature", "../../assets/images/creature_symbol.svg"),
+    new FilterButton("enchantment", "../../assets/images/enchantment_symbol.svg"),
+    new FilterButton("land", "../../assets/images/land_symbol.svg"),
+    new FilterButton("planeswalker", "../../assets/images/planeswalker_symbol.svg"),
+    new FilterButton("sorcery", "../../assets/images/sorcery_symbol.svg"),
+    new FilterButton("instant", "../../assets/images/instant_symbol.svg")
+  ]
 
 }
