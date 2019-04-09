7642317cb2a0167bf97bc5752baef8e1a963918f
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-04-09 14:07:30 -0700

    add class color change and button styling on details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
+  Test: any = {
+    name: "Cabal Therapist",
+    rarity: "rare",
+    colors: ["U"],
+    set_name: "Modern Horizons",
+    mana_cost: "{B}",
+    prices: {eur:"14.89", usd: "15.83", usdFoil: "49.99"},
+    type_line: "Creature — Horror",
+    power: "1",
+    toughness: "1",
+    oracle_text: "Menace\nAt the beginning of your precombat main ..."
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  color(card){
+    let htmlClass: string;
+    if(card.colors[1]){
+      htmlClass = "multi";
+    } else{
+      htmlClass = card.colors[0];
+    }
+    return htmlClass;
   }
 
 }
