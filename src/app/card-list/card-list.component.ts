25259f118edc8879f2d387a4f91074e3e8002b54
Student <example@example.com>
2019-04-10 11:57:35 -0700

    altering api flow

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -17,38 +17,39 @@ export class CardListComponent implements OnInit {
   cardPage: any[] = [];
   cardList: any[] = [];
   hasMore: boolean = false;
+  allPages;
   constructor(private cardsService: MtgCardsService, private dbService: DbService) { }
 
   ngOnInit() {
-    let timerId = setTimeout(function tick() {
-      console.log('tick');
-      timerId = setTimeout(tick, 5000);
-    }, 5000);
   }
 
   getMTGcards() {
-    this.cardsService.getMTGCardList().subscribe(response => {
-      this.cardPage = response.json().data;
-      this.cardList.push(this.cardPage);
-      this.getNextPage();
+   this.cardsService.getMTGCardList().subscribe(response => {
+      this.cardPage = response.json();
+      this.cardList = [].concat.apply([], response.json().data);
+      console.log('cardPage', this.cardPage);
+      console.log('cardPage has more', this.cardPage['has_more']);
+      console.log('cardPage next_page', this.cardPage['next_page']);
+      console.log('cardList', this.cardList);
     });
   }
 
   getNextPage(){
-    for (let i=2; i < (5) ; i++) {
-      setTimeout( () => {
-        let nextPage = `https://api.scryfall.com/cards?lang=en&page=${i}`;
-        this.cardsService.getMTGNextPage(nextPage).subscribe(response => {
-          this.cardPage = response.json().data;
-          this.cardList.push(this.cardPage);
-        });
-      }, 100);
-    }
-    setTimeout ( () => {
-      let allPages = [].concat.apply([], this.cardList);
-      console.log(allPages);
-    }, 3000);
+    let nextPage = this.cardPage['next_page'];
+    this.cardsService.getMTGNextPage(nextPage).subscribe(response => {
+      this.cardPage = response.json();
+      this.cardList = this.cardList.concat.apply(this.cardList, response.json().data);
+      // this.cardList.push(response.json().data);
+      console.log('new cardPage', this.cardPage);
+      console.log('new longer list', this.cardList);
+    });
   }
+
+  getShallowCardInfo() {
+    //name supertype color
+    console.log(this.cardList[0].name + this.cardList[0].mana_cost);
+  }
+
 
   saveMTGcards(cardlist) {
     this.dbService.writeCards(cardlist);
