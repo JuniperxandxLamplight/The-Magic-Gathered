28295c0a2ed4ee1719a0755c1e80a186ee39ae22
Kimberly McConnell <kmcconnell393@gmail.com>
2019-04-10 16:26:19 -0700

    fix register current user issue

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,13 +3,18 @@ import { LoginComponent } from '../login/login.component';
 import { AuthenticationService } from '../authentication.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserService } from '../user.service'
+import { User } from '../models/user-data.model'
+
+
+
 
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
-  providers: [AuthenticationService]
+  providers: [AuthenticationService, UserService]
 })
 
 export class RegisterComponent implements OnInit {
@@ -17,8 +22,9 @@ export class RegisterComponent implements OnInit {
   errorMessage;
   successMessage;
   loginSuccess = null;
+  private username: string;
 
-  constructor(public authService: AuthenticationService, private formBuilder: FormBuilder, private router: Router, private loginAdd: LoginComponent) { }
+  constructor(public authService: AuthenticationService, private formBuilder: FormBuilder, private router: Router, private userService: UserService) { }
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
@@ -36,12 +42,36 @@ export class RegisterComponent implements OnInit {
       this.loginSuccess = true;
       this.errorMessage = "";
       this.successMessage = alert("Your account has been created");
-      this.loginAdd.addUser();
+      this.addUser(value.username);
       this.router.navigate(['/user', value.username]);
     }, err => {
       console.log(err);
       this.errorMessage = err.message;
       this.successMessage = "";
     })
+  }
+
+  addUser(name) {
+    let newUser = new User(name, [], []);
+    console.log("newUser", newUser.library);
+    this.userService.addUserToDB(newUser);
+    // let allUserNames: any[] = [];
+    // this.userService.getUsers().subscribe( list => {
+    //   console.log("userlist", list);
+    //
+    //   list.forEach((user) => {
+    //     allUserNames.push(user.username);
+    //     console.log("allusernames", allUserNames);
+    //   });
+    //
+    //   if (allUserNames.includes(this.username)) {
+    //     console.log("notNew");
+    //   }
+    //   else{
+    //     let newUser = new User(this.username, [], []);
+    //     console.log("newUser", newUser.library);
+    //     this.userService.addUserToDB(newUser);
+    //   }
+    // })
   }
 }
