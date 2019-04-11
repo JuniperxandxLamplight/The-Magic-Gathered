7b5bf84d0efdf5e3e968c7fac5916149abffd6f9
Student <example@example.com>
2019-04-11 11:27:22 -0700

    finish styling banner displays and add colorless

--- src/app/deck-list/deck-list.component.ts
+++ src/app/deck-list/deck-list.component.ts
@@ -19,7 +19,7 @@ export class DeckListComponent implements OnInit {Test: any;
         this.Test = {
         name: "Cabal Therapist",
         rarity: "rare",
-        colors: ["U"],
+        colors: ["U", "R", "B", "G", "W"],
         set_name: "Modern Horizons",
         mana_cost: "{B}",
         cmc: "1",
@@ -33,7 +33,7 @@ export class DeckListComponent implements OnInit {Test: any;
         this.Test = {
         name: "Green Dude",
         rarity: "rare",
-        colors: ["G"],
+        colors: ["G", "R", "W", "B"],
         set_name: "Modern Horizons",
         mana_cost: "{B}",
         cmc: "3",
@@ -45,7 +45,7 @@ export class DeckListComponent implements OnInit {Test: any;
       });
       this.testList.push(
         this.Test = {
-        name: "Red Dude",
+        name: "duuuuuuuude",
         rarity: "rare",
         colors: ["R"],
         set_name: "Modern Horizons",
@@ -57,24 +57,133 @@ export class DeckListComponent implements OnInit {Test: any;
         toughness: "5",
         oracle_text: "Menace\nAt the beginning of your precombat main ..."
       });
+      this.testList.push(
+        this.Test = {
+        name: "duuuuuuuude",
+        rarity: "rare",
+        colors: ["U"],
+        set_name: "Modern Horizons",
+        mana_cost: "{B}",
+        cmc: "8",
+        prices: {eur:"14.89", usd: "15.83", usdFoil: "49.99"},
+        type: "Creature",
+        power: "5",
+        toughness: "5",
+        oracle_text: "Menace\nAt the beginning of your precombat main ..."
+      });
+      this.testList.push(
+        this.Test = {
+        name: "duuuuuuuude",
+        rarity: "rare",
+        colors: ["G"],
+        set_name: "Modern Horizons",
+        mana_cost: "{B}",
+        cmc: "8",
+        prices: {eur:"14.89", usd: "15.83", usdFoil: "49.99"},
+        type: "Creature",
+        power: "5",
+        toughness: "5",
+        oracle_text: "Menace\nAt the beginning of your precombat main ..."
+      });
+      this.testList.push(
+        this.Test = {
+        name: "duuuuuuuude",
+        rarity: "rare",
+        colors: [],
+        set_name: "Modern Horizons",
+        mana_cost: "{B}",
+        cmc: "8",
+        prices: {eur:"14.89", usd: "15.83", usdFoil: "49.99"},
+        type: "Artifact",
+        power: "5",
+        toughness: "5",
+        oracle_text: "Menace\nAt the beginning of your precombat main ..."
+      });
+      this.testList.push(
+        this.Test = {
+        name: "duuuuuuuude",
+        rarity: "rare",
+        colors: ["B"],
+        set_name: "Modern Horizons",
+        mana_cost: "{B}",
+        cmc: "8",
+        prices: {eur:"14.89", usd: "15.83", usdFoil: "49.99"},
+        type: "Creature",
+        power: "5",
+        toughness: "5",
+        oracle_text: "Menace\nAt the beginning of your precombat main ..."
+      });
+      this.testList.push(
+        this.Test = {
+        name: "duuuuuuuude",
+        rarity: "rare",
+        colors: ["W"],
+        set_name: "Modern Horizons",
+        mana_cost: "{B}",
+        cmc: "8",
+        prices: {eur:"14.89", usd: "15.83", usdFoil: "49.99"},
+        type: "Creature",
+        power: "5",
+        toughness: "5",
+        oracle_text: "Menace\nAt the beginning of your precombat main ..."
+      });
     }
-
-    // for(let i=0; i>this.testList.length; i++){
-    //   const card = document.getElementById(`${i}`);
-    //   console.log(i);
-    //   card.setAttribute('top', `${i * 20}`);
-    // }
   }
 
   color(card){
     let htmlClass: string;
     if(card.colors[1]){
       htmlClass = "multi";
+    } else if(card.colors.length === 0){
+      htmlClass = "colorless"
     } else{
       htmlClass = card.colors[0];
     }
     return htmlClass;
   }
+  checkCover(card){
+    let htmlClass: string;
+    if(card.colors[1]){
+      htmlClass = "multi" + "Cover";
+    } else if(card.colors.length === 0){
+      htmlClass = "colorless" + "Cover";
+    }else{
+      htmlClass = card.colors[0] + "Cover";
+    }
+    return htmlClass;
+  }
+  colorIcon(card, i){
+    let colors: string;
+    if(card.colors[4]){
+      colors = "multi";
+      return colors;
+    } else{
+      return card.colors[i];
+    }
+  }
+  colorImgClass(card, i){
+    let colors: string;
+    if(card.colors[4]){
+      colors = "multi";
+      return "img" + colors + i;
+    } else{
+      return "img" + card.colors[i];
+    }
+  }
 
+  imgMove(i){
+    const leftMove: number = i * 50;
+    return leftMove;
+  }
+
+  trunk(name){
+    if(name.charAt(10)){
+      let shortStr: string = name.substring(0, 11);
+      let newStr: string = shortStr + "...";
+      return newStr;
+    } else{
+      return name;
+    }
+  }
 
 }
